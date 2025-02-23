598c12cef11c2f1e54965c78b9c1059d0b8315ba
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2025-02-23 14:01:58 -0500

    a

--- sim.py
+++ sim.py
@@ -1,5 +1,6 @@
 import jsbsim
 import time
+from tabulate import tabulate
 
 # Ref: https://github.com/AOS55/Fixedwing-Airsim/blob/master/src/jsbsim_properties.py
 
@@ -8,31 +9,47 @@ class Simulator:
         self.fdm = jsbsim.FGFDMExec("models_jsbsim", None)  # Use JSBSim default aircraft data.
         self.fdm.set_dt(0.005) # Run sim in background at 200hz
         self.start_time = time.time()
+        self.last_print_time = time.time()
 
         # Load aircraft model
         self.fdm.load_model("Rascal110")
 
         self.set_initial_conditions()
+        self.fdm.run_ic()
     
     def set_initial_conditions(self):
         self.fdm["ic/lat-geod-deg"] = 38.897957
         self.fdm["ic/long-gc-deg"] = -77.036560
-        self.fdm["ic/h-sl-ft"] = 10
+        self.fdm["ic/h-sl-ft"] = 50
         self.fdm['ic/psi-true-deg'] = 0
-        self.fdm["ic/vc-kts"] = 0
-        self.fdm["propulsion/set-running"] = -1
+        self.fdm["ic/vc-kts"] = 50
 
     def run(self):
         sim_time = self.fdm.get_sim_time()
         real_time = time.time() - self.start_time
 
         if real_time >= sim_time:
+            self.fdm['fcs/aileron-cmd-norm'] = 0.02 * (0 - self.fdm['attitude/phi-deg']) # P controller wing leveller
+            self.fdm['fcs/elevator-cmd-norm'] = -1 * 0.02 * (10 - self.fdm['attitude/theta-deg'])
+            self.fdm['fcs/throttle-cmd-norm'] = 0.9
+
             self.fdm.run()
 
-            print(
-                f"{sim_time:.3f}\t"
-                f"{self.fdm.get_property_value('attitude/psi-deg'):.1f}\t"
-                f"{self.fdm.get_property_value('position/h-sl-ft'):.2f}\t"
-                f"{self.fdm.get_property_value('position/lat-geod-deg'):.7f}\t"
-                f"{self.fdm.get_property_value('position/long-gc-deg'):.7f}\t"
-            )
+            if time.time() - self.last_print_time > 0.4:
+                headers = ["Time", "Roll", "Pitch", "Heading", "Alt", "Lat", "Lon", "Spd", "Thr"]
+                data = [[
+                    f"{sim_time:.1f}",
+                    f"{self.fdm['attitude/phi-deg']:.1f}",
+                    f"{self.fdm['attitude/theta-deg']:.1f}",
+                    f"{self.fdm['attitude/psi-deg']:.1f}",
+                    f"{self.fdm['position/h-sl-ft']:.1f}",
+                    f"{self.fdm['position/lat-geod-deg']:.7f}",
+                    f"{self.fdm['position/long-gc-deg']:.7f}",
+                    f"{self.fdm['velocities/vc-kts']:.1f}",
+                    f"{self.fdm['fcs/throttle-cmd-norm']:.2f}"
+                ]]
+                
+                print(tabulate(data, headers=headers, tablefmt="plain"))
+                print()
+                
+                self.last_print_time = time.time()
